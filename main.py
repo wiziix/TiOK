0b90bdb91b1c5e2f4027fa3f6aa7af760ffe506f
Kacper <fexi1339@gmail.com>
2024-03-19 14:57:45 +0100

    completed albums page, usernames, album titles and covers are now displayed

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import requests
-from flask import Flask
-from flask import render_template, request
+from flask import Flask, render_template, request
 import json
 
-response = requests.get("https://jsonplaceholder.typicode.com/albums/1")
-
 users = requests.get("https://jsonplaceholder.typicode.com/users")
-
 user = json.loads(users.content)
 
+res = requests.get("https://jsonplaceholder.typicode.com/photos/")
+photos = json.loads(res.content)
+
+#album thumbnails dictionary
+#where keys are albumIDs
+#and values are thumbnail URLs
+thumbnails = {}
+
+for i in range(0,len(photos), 50):
+    thumbnails[photos[i]['albumId']] = photos[i]['thumbnailUrl']
+
 #usernames dictionary
-#keys are userIDs
+#where keys are userIDs
 #values are usernames
 usernames = {}
 
@@ -27,8 +34,12 @@ def hello_world():
     return render_template('home_page.html')
 @app.route("/albums", methods=['GET'])
 def Albums():
-    response = requests.get("https://jsonplaceholder.typicode.com/albums/1")
-    y = json.loads(response.content)
+    response = requests.get("https://jsonplaceholder.typicode.com/albums/")
+    album_content  = json.loads(response.content)
 
-    return render_template('albums.html', username = usernames[int(y['userId'])],
-                           title = y['title'])
+
+    return render_template('albums.html',
+                           albums = album_content,
+                           username = usernames,
+                           cover = thumbnails)
+
