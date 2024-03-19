64da260b59310298ff6d2c85fce8978ea755381a
Student <fexi1339@gmail.com>
2024-03-19 09:24:24 +0100

    added usernames dictionary, filled some of the table rows of 'albums' section

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import requests
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+import json
 
-response = requests.get("https://jsonplaceholder.typicode.com/")
-print(response)
+response = requests.get("https://jsonplaceholder.typicode.com/albums/1")
+
+users = requests.get("https://jsonplaceholder.typicode.com/users")
+
+user = json.loads(users.content)
+
+#usernames dictionary
+#keys are userIDs
+#values are usernames
+usernames = {}
+
+#filling the usernames dict
+for i in range(10):
+    usernames[user[i]['id']] = user[i]['username']
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template('home_page.html')
-@app.route("/albums")
+@app.route("/albums", methods=['GET'])
 def Albums():
-    return render_template('albums.html')
+    response = requests.get("https://jsonplaceholder.typicode.com/albums/1")
+    y = json.loads(response.content)
+
+    return render_template('albums.html', username = usernames[int(y['userId'])],
+                           title = y['title'])
