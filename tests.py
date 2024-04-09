c1a5b273ef0f02e904c94a86d10448b03443d9e2
Student <fexi1339@gmail.com>
2024-04-09 09:42:29 +0200

    completed few tests

--- tests.py
+++ tests.py
@@ -1,6 +1,33 @@
 from unittest.mock import patch, MagicMock
-import requests
+
+import main
+
 
 def test_getUserData():
 
-    response =
+    request_mock = MagicMock(return_value = MagicMock(json = lambda: {"username" : "Bret"}))
+
+    with patch('requests.get', request_mock):
+
+        data = main.getUserData().json()
+
+    request_mock.assert_called_once_with("https://jsonplaceholder.typicode.com/users")
+
+    assert data == {"username" : "Bret"}, "Funkcja getUserData nie zwrocila oczekiwanych danych"
+
+
+def test_getUsernames():
+
+    function_mock = MagicMock(return_value = {1: 'Bret'})
+
+    with patch('main.getUsernames', function_mock):
+        result = main.getUsernames(main.getUserData()).get(1)
+
+    assert result == 'Bret', "Funkcja nie zwróciła wartości Bret"
+
+    function_mock.assert_called_once()
+
+test_getUserData()
+test_getUsernames()
+
+
