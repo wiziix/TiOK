0f895e2f81a7a4fc9861949ca2cce3bce2f7a8a0
Student <fexi1339@gmail.com>
2024-05-21 09:46:33 +0200

    fixed albums loading

--- tests/test_albums.py
+++ tests/test_albums.py
@@ -5,14 +5,16 @@ import re
 def test_albums():
     flask_app = app
     username_pattern = "<td>(.*?)</td>"
-    photo_pattern = r'https:\/\/via\.placeholder\.com\/150\/[0-9a-fA-F]{6}'
+    photo_pattern = r"https:\/\/via\.placeholder\.com\/150\/[0-9a-fA-F]{6}"
 
     with flask_app.test_client() as test_client:
-        response = test_client.get('/albums')
+        response = test_client.get("/albums")
         match_results = re.findall(username_pattern, response.text, re.IGNORECASE)
         assert response.status_code == 200
-        assert match_results[0] == 'Bret'
-        assert match_results[-2] == 'Moriah.Stanton'
-        match_results2 = re.findall(photo_pattern, response.text, re.IGNORECASE | re.DOTALL)
-        assert match_results2[0] == 'https://via.placeholder.com/150/92c952'
-        assert match_results2[-1] == 'https://via.placeholder.com/150/92bfbf'
+        assert match_results[0] == "Bret"
+        assert match_results[-2] == "Moriah.Stanton"
+        match_results2 = re.findall(
+            photo_pattern, response.text, re.IGNORECASE | re.DOTALL
+        )
+        assert match_results2[0] == "https://via.placeholder.com/150/92c952"
+        assert match_results2[-1] == "https://via.placeholder.com/150/92bfbf"
--- tests/test_getThumbnails.py
+++ tests/test_getThumbnails.py
@@ -3,11 +3,15 @@ from unittest.mock import patch, MagicMock
 
 def test_getThumbnails():
 
-    function_mock = MagicMock(return_value = {1: "https://via.placeholder.com/150/92c952"})
+    function_mock = MagicMock(
+        return_value = {1: "https://via.placeholder.com/150/92c952"}
+    )
 
-    with patch('main.getThumbnails', function_mock):
+    with patch("main.getThumbnails", function_mock):
         result = main.getThumbnails().get(1)
 
-    assert result == "https://via.placeholder.com/150/92c952", "Funkcja nie zwrocila oczekiwanych danych"
+    assert (
+        result == "https://via.placeholder.com/150/92c952"
+    ), "Funkcja nie zwrocila oczekiwanych danych"
 
     function_mock.assert_called_once()
